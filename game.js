1a352f6c07375ef278ad020dbaa122a8ac2c183e
Luan Goncalves <luanarcharde@gmail.com>
2021-03-20 13:31:38 +0100

    Create the game file and add initial game values

---
+++ game.js
@@ -0,0 +1,30 @@
+
+
+//Game Values --------------------------------------------------------------------------------------------------
+
+let lastRenderTime = 0;
+
+
+function main(currentTime) {
+  
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / snakeSpeed) return;
+
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  
+}
+
+function draw() {
+ 
+}
+
